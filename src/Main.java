5325a1e315f323dacacb2ace54db1e1035bb7e64
MagedNatCs <magednat@post.bgu.ac.il>
2023-04-17 01:23:37 +0300

    printing donkey

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,15 @@ public class Main {
 
         System.out.println("bashar is good");
         Prints_2();
+        Prints_Donkey();
     }
 
 
     public static void Prints_2(){
         System.out.println("2");
+    }
+    public static void Prints_Donkey(){
+        System.out.println("donkey!");
     }
 }
 
